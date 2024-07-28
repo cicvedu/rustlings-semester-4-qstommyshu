07a835310a0341be28e2c6f4a1a7ac057ca4fd2d
qstommyshu <qstommyshu@gmail.com>
2024-07-28 10:40:50 -0400

    finish up to question 90

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -30,6 +28,9 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
+    // TODO: create multiple sender
+    // TODO: need to think about this file
+    let tx1 = tx.clone();
 
     thread::spawn(move || {
         for val in &qc1.first_half {
@@ -42,7 +43,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
